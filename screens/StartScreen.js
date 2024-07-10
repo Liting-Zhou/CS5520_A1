3c2aac2469c57899b5bd61102ad4d6324f2a15ad
Liting-Zhou <143493320+Liting-Zhou@users.noreply.github.com>
2024-07-10 16:49:47 -0700

    construct StartScreen.js

---
+++ screens/StartScreen.js
@@ -0,0 +1,81 @@
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React from "react";
+import CheckBox from "@react-native-community/checkbox";
+
+export default function StartScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome</Text>
+      <View style={styles.card}>
+        <View style={styles.topContainer}>
+          <Text>Name</Text>
+          <TextInput style={styles.textInputStyle}></TextInput>
+          <Text>Email address</Text>
+          <TextInput style={styles.textInputStyle}></TextInput>
+        </View>
+        <View style={styles.robotContainer}>
+          <CheckBox disabled={false} />
+          <Text>I am not a robot</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Reset"
+            onPress={() => {
+              console.log("StartScreen 19");
+            }}
+            color={"darkred"}
+          ></Button>
+          <Button
+            title="Start"
+            onPress={() => {
+              console.log("StartScreen 25");
+            }}
+            color={"ivory"}
+          ></Button>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
+    // width: "60%",
+  },
+  card: {
+    backgroundColor: "grey",
+    flex: 0.5,
+    padding: 10,
+    borderRadius: 5,
+    width: "70%",
+  },
+  textInputStyle: {
+    height: 40,
+    borderColor: "blue",
+    borderBottomWidth: 1,
+    marginBottom: 30,
+    // width: "100%",
+    // padding: 5,
+  },
+  topContainer: {
+    flex: 3,
+    padding: 10,
+  },
+  robotContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+    // justifyContent: "center",
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10,
+  },
+});
